e7ec316c4b4a533794201f8dc5d20da5d9b2e71a
GuardianOfAngel <neerajramachandran30@gmail.com>
2021-06-01 17:36:03 +0530

    Fireb

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYQU8zvtzJKwAAqhGN95AnZBPyjaWPCTk",
+    authDomain: "movie-freakz.firebaseapp.com",
+    projectId: "movie-freakz",
+    storageBucket: "movie-freakz.appspot.com",
+    messagingSenderId: "590576888782",
+    appId: "1:590576888782:web:834221de29cd5507a3599f"
+  };
